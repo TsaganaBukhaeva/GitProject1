b904243ccdf4184f2d8e8c7cbf961f0d4e2b0e0b
tsaganabukhaeva <Angira8080@gmail.com>
2022-07-18 18:37:53 -0400

    added more Hello feature

--- src/test/java/work1/Hello.java
+++ src/test/java/work1/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
         System.out.println("Work started");
         System.out.println("Created Git Repository");
         System.out.println("Developer hello feature");
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
     }
 
 }
