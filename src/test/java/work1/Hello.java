295a7f733926834d95af5aec9a431c7d3ce7bdaf
tsaganabukhaeva <Angira8080@gmail.com>
2022-07-18 16:58:50 -0400

    added more work

--- src/test/java/work1/Hello.java
+++ src/test/java/work1/Hello.java
@@ -3,6 +3,7 @@ package work1;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Work started");
+        System.out.println("Created Git Repository");
     }
 
 }
